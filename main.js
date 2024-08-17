d57391d62f85dd5ceb4d317ae7f889950f1221b3
MarcinJaracz <mj24up@gmail.com>
2024-08-17 13:40:48 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Odpowiednik __filename
+const __filename = fileURLToPath(import.meta.url);
+
+// Odpowiednik __dirname
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
